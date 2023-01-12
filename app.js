f418ea951a54a5079021ca949a506ea518d41768
arvin0181 <arvin.aditama@gmail.com>
2023-01-12 17:50:22 +0700

    update-login-clear

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router  = require('./routes/index');
 const app = express()
+const session = require('express-session')
 const port = 3000
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }))
 
 app.use ('/',router)
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,26 @@ const express = require('express')
 const Controller = require('../Controller/controller')
 const router = express.Router()
 const admin = require('./admin')
+
 const user = require('./user')
+
+
+
+// router.use((req, res, next) => {
+//   console.log('Time:', Date.now())
+//   next()
+// })
+const coba =function (req, res, next) {
+  console.log('Time:', Date.now())
+  next()
+}
+router.post('/login',coba, Controller.handlerLogin)
+
+
 
 router.get('/', Controller.home)
 
-router.post('/login', Controller.handlerLogin)
+
 
 router.use('/admins', admin)
 router.use('/users', user)
